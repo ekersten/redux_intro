b50bdd20b026c7307672e6bb96bf2393c387d147
Eric Kersten <ekersten@gmail.com>
2017-11-22 18:22:00 -0300

    extract text config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
@@ -13,13 +14,16 @@ module.exports = {
             exclude: /node_modules/
         }, {
             test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                //resolve-url-loader may be chained before sass-loader if necessary
+                use: ['css-loader', 'sass-loader']
+            })
         }]
     },
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ],
     devtool: 'nosources-source-map',
     'devServer': {
         contentBase: path.join(__dirname, 'public')
