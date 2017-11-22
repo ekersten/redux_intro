a5add224e51438318a3dcb3b99630de2273ed6fb
Eric Kersten <ekersten@gmail.com>
2017-11-22 17:54:51 -0300

    copied files from tutorial

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,31 +1,29 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
-    state = {
-        error: undefined
+  state = {
+    error: undefined
+  };
+  handleAddOption = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
     }
-    handleOnSubmit = (e) => {
-        e.preventDefault();
-        const option = e.target.elements.optionText.value.trim();
-        const error = this.props.handleAddOption(option);
-        
-        if(error) {
-            this.setState(() => ({error}));
-        } else {
-            e.target.reset();
-            e.target.elements.optionText.focus();
-        }
-    }
-    render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>  }
-                <form onSubmit={this.handleOnSubmit}>
-                    <input type="text" name="optionText" />
-                    <button>Add Option</button>
-                </form>
-            </div>
-            
-        )
-    }
+  };
+  render() {
+    return (
+      <div>
+        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+        <form className="add-option" onSubmit={this.handleAddOption}>
+          <input className="add-option__input" type="text" name="option" />
+          <button className="button">Add Option</button>
+        </form>
+      </div>
+    );
+  }
 }
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,18 +2,29 @@ import React from 'react';
 import Option from './Option';
 
 const Options = (props) => (
-    <div>
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length == 0 && <p>Enter an option</p>}
-        {
-            props.options.map((option, index) => (
-                <Option
-                    optionText={option}
-                    handleDeleteOption={props.handleDeleteOption}
-                />
-            ))
-        }
+  <div>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+    </button>
     </div>
-)
 
-export default Options
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
+    {
+      props.options.map((option, index) => (
+        <Option
+          key={option}
+          optionText={option}
+          count={index + 1}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
+  </div>
+);
+
+export default Options;
