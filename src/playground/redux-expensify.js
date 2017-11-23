60d463f7386b1ec6fda4f0b5a88ebaf1a30a85f0
Eric Kersten <ekersten@gmail.com>
2017-11-23 16:58:09 -0300

    spread object and set sort

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import uuid from 'uuid';
 
-// Action generators
+// ADD_EXPENSE
 const addExpense = ({ description = '', note = '', ammount = 0, createdAt = 0 } = {}) => ({
     type: 'ADD_EXPENSE',
     expense: {
@@ -13,10 +13,35 @@ const addExpense = ({ description = '', note = '', ammount = 0, createdAt = 0 } 
     }
 })
 
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+})
+
+// REMOVE_EXPENSE
 const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id: id
 })
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+})
+
+// SORT_BY_DATE
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+})
+// SORT_BY_AMMOUNT
+const sortByAmmount = () => ({
+    type: 'SORT_BY_AMMOUNT'
+})
+// SET_START_DATE
+// SET_END_DATE
 
 // Expenses reducer
 const expensesReducerDefaultState = []
@@ -27,7 +52,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
                 ...state,
                 action.expense
             ]
-
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                } else {
+                    return expense;
+                }
+            });
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id);
 
@@ -35,6 +70,7 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             return state;
     }
 }
+
 
 // Filters reducer
 const filtersReducerDefaultState = {
@@ -45,6 +81,24 @@ const filtersReducerDefaultState = {
 }
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
+
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            }
+
+        case 'SORT_BY_AMMOUNT':
+            return {
+                ...state,
+                sortBy: 'ammount'
+            }
+
         default:
             return state;
     }
@@ -66,3 +120,10 @@ const expenseOne = store.dispatch(addExpense({ description: 'Rent', ammount: 100
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', ammount: 300 }));
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { ammount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
+store.dispatch(sortByAmmount());
+store.dispatch(sortByDate());
