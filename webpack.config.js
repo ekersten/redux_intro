2ef4316f830d6a4bd0d2be92349b214266868361
Eric Kersten <ekersten@gmail.com>
2017-11-23 15:18:00 -0300

    action generators

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/playground/destructuring.js',
+    entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
